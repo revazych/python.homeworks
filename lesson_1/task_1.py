1fccc685d1ae92f476f9f1fe02a5e91ed6a33502
gvr <gvazava86@yandex.ru>
2021-12-01 00:16:02 +0300

    Added homework_1

---
+++ lesson_1/task_1.py
@@ -0,0 +1,9 @@
+# number_1 = input('Введите число ')
+# number_2 = input('Введите число ')
+# number_3 = input('Введите число ')
+# str_1 = input('Введите слово ')
+# str_2 = input('Введите букву ')
+#
+# result = number_1, number_2, number_3, str_1, str_2
+# print(result)
+#
---
+++ lesson_1/task_3.py
@@ -0,0 +1,8 @@
+# string_1 = input('Введите число ')
+# string_2 = string_1 * 2
+# string_3 = string_1 * 3
+#
+# result = int(string_1) + int(string_2) + int(string_3)
+#
+# print(result)
+#
