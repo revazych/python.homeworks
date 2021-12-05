f3af0910cb0d64b5982ba8491bfc355cc71018d2
gvr <gvazava86@yandex.ru>
2021-12-06 02:51:09 +0300

    Added homework_2

---
+++ task_3.py
@@ -0,0 +1,10 @@
+# wnt, spr, sum, fal = 'зима', 'весна', 'лето', 'осень'
+# year = [wnt, wnt, spr, spr, spr, sum, sum, sum, fal, fal, fal, wnt]
+# month_number = int(input('Введите порядковый номер месяца '))
+# print(year[int(month_number)-1])
+#
+
+# year = {1: 'зима', 2: 'зима', 3: 'весна', 4: 'весна', 5: 'весна', 6: 'лето', 7: 'лето', 8: 'лето', 9: 'осень', 10: 'осень', 11: 'осень', 12: 'зима'}
+# month_number = int(input('Введите порядковый номер месяца '))
+# print(year[month_number])
+#
